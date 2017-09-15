6bf5e6a4ff94b636455053920834b13b9162cd93
Ryan Smith <0ryansmith1994@gmail.com>
2017-09-15 17:26:57 +0100

    fix: Removes old testing details.

--- src/presenter/express/visitLink/visitLink.test.ts
+++ src/presenter/express/visitLink/visitLink.test.ts
@@ -7,9 +7,9 @@ describe(__filename, () => {
   it('should return a 307 when visiting an existing short url', async () => {
     const shortUrl = 'test_short_url';
     const { id: clientId } = await service.createClient({
-      lrsEndpoint: 'http://devv2.learninglocker.net/data/xAPI',
-      lrsKey: '77a26a0be679a55d29e40203b1e10088c8986ce6',
-      lrsSecret: 'f3c9e544128390af43b8baa80b24b4cabfe9aee6',
+      lrsEndpoint: 'test_endpoint',
+      lrsKey: 'test_key',
+      lrsSecret: 'test_secret',
       name: 'test_client',
     });
     await service.createLink({
