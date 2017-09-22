00427745c16f3382511c1dafdc6fb8c3b042bd99
Ryan Smith <0ryansmith1994@gmail.com>
2017-09-22 08:29:58 +0200

    feat(service.trackLink): Adds location info to statement from IP Address.

---
+++ @types/iplocation/index.d.ts
@@ -0,0 +1,18 @@
+declare module 'iplocation' {
+  interface IpLocation {
+    ip: string;
+    country_code: string;
+    country_name: string;
+    region_code: string;
+    region_name: string;
+    city: string;
+    zip_code: string;
+    time_zone: string;
+    latitude: number;
+    longitude: number;
+    metro_code: number;
+  }
+
+  const x: (ipAddress?: string) => IpLocation;
+  export = x;
+}
--- src/service/trackLink/service.ts
+++ src/service/trackLink/service.ts
@@ -1,3 +1,4 @@
+import * as iplocation from 'iplocation';
 import { defaultTo } from 'ramda';
 import Config from '../utils/Config';
 import Signature from './Signature';
@@ -7,6 +8,7 @@ const HOME_PAGE = 'https://github.com/LearningLocker/url-shortener';
 export default (config: Config): Signature => {
   return async ({ shortUrl, ipAddress, userAgent }) => {
     const { longUrl, clientId } = await config.repo.getLinkByShortUrl({ shortUrl });
+    const location = await iplocation(ipAddress);
     const statement: any = {
       actor: {
         account: {
@@ -23,6 +25,16 @@ export default (config: Config): Signature => {
           [`${HOME_PAGE}/useragent/os/family`]: userAgent.os.family,
           [`${HOME_PAGE}/useragent/os/source`]: userAgent.os.toString(),
           [`${HOME_PAGE}/useragent/os/version`]: Number(userAgent.os.major),
+          [`${HOME_PAGE}/useragent/location/countryCode`]: location.country_code,
+          [`${HOME_PAGE}/useragent/location/countryName`]: location.country_name,
+          [`${HOME_PAGE}/useragent/location/regionCode`]: location.region_code,
+          [`${HOME_PAGE}/useragent/location/regionName`]: location.region_name,
+          [`${HOME_PAGE}/useragent/location/city`]: location.city,
+          [`${HOME_PAGE}/useragent/location/zipCode`]: location.zip_code,
+          [`${HOME_PAGE}/useragent/location/timeZone`]: location.time_zone,
+          [`${HOME_PAGE}/useragent/location/latitude`]: location.latitude,
+          [`${HOME_PAGE}/useragent/location/longitude`]: location.longitude,
+          [`${HOME_PAGE}/useragent/location/metroCode`]: location.metro_code,
         },
       },
       object: {
@@ -39,6 +51,7 @@ export default (config: Config): Signature => {
         id: 'http://activitystrea.ms/schema/1.0/experience',
       },
     };
+    /* console.log(JSON.stringify(statement, null, 2)); */
     const { lrsEndpoint, lrsKey, lrsSecret } = await config.repo.getClientById({ id: clientId });
     config.repo.sendStatement({ statement, lrsEndpoint, lrsKey, lrsSecret })
       .then(() => {
